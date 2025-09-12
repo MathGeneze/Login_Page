55e178e78edffcdc95836fcc9b6b1b9bb7ecd8fe
Matheus Geneze <matheus.geneze@gmail.com>
2025-09-11 23:05:31 -0300

    Update navegation.py

--- navegation.py
+++ navegation.py
@@ -3,13 +3,13 @@ import streamlit as st
 pg = st.navigation([
     
     # Página de login
-    st.Page('pages/login.py', title='Login', url_path='Login do Usuário'),
+    st.Page('pages/login.py', title='Login', url_path='Login do Usuário', icon=':material/login:'),
     
     # Página de criação de conta
-    st.Page('pages/registro.py', title='Criando uma conta', url_path='create_account'),
+    st.Page('pages/registro.py', title='Criando uma conta', url_path='create_account', icon=':material/account_circle:'),
     
     # página de recuperação de senha
-    st.Page('pages/password.py', title='Recuperação de senha', url_path='recuperação_de_senha')
+    st.Page('pages/password.py', title='Recuperação de senha', url_path='recuperação_de_senha', icon=':material/password:')
 ])
 
 pg.run()
